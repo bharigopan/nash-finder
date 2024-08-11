3a25c4ba2bd7ec90885eb4dca2bfbbf182f0c470
bharigopan <46465452+bharigopan@users.noreply.github.com>
2024-08-11 08:28:24 +0530

    Update example_basic.py

--- example_basic.py
+++ example_basic.py
@@ -10,25 +10,25 @@ game.player_init_mixed_strategies()
 """
 Prosoner's dilemma
 Bimatrix:
-    [-10, -1],
-    [-25, -3]
+    [-100000, -10000],
+    [-250000, -30000]
 
-    [-10, -25],
-    [-1, -3],
-the unique NE at (-10, -10)
+    [-100000, -250000],
+    [-10000, -30000],
+the unique NE at (-100000, -100000)
 """
 
 # player 1
-game.player_assign_payoff(1, "11", -10)
-game.player_assign_payoff(1, "12", -1)
-game.player_assign_payoff(1, "21", -25)
-game.player_assign_payoff(1, "22", -3)
+game.player_assign_payoff(1, "11", -100000)
+game.player_assign_payoff(1, "12", -10000)
+game.player_assign_payoff(1, "21", -250000)
+game.player_assign_payoff(1, "22", -30000)
 
 # player 2
-game.player_assign_payoff(2, "11", -10)
-game.player_assign_payoff(2, "12", -25)
-game.player_assign_payoff(2, "21", -1)
-game.player_assign_payoff(2, "22", -3)
+game.player_assign_payoff(2, "11", -100000)
+game.player_assign_payoff(2, "12", -250000)
+game.player_assign_payoff(2, "21", -10000)
+game.player_assign_payoff(2, "22", -30000)
 
 # run the iterations to approximate Nash equilibrium
 game.run()
